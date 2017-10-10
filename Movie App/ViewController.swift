08f04140af63c002b732eeaec60046fb86bc6294
Adrian Brown <adrianb@bighuman.com>
2017-10-10 17:57:11 -0400

    chore: initial collectionView setup-testing

--- Movie App/ViewController.swift
+++ Movie App/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +25,19 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath);
+        
+        return cell
+    }
+    
+    
+}
---
+++ Movie App/omdbClient.swift
@@ -0,0 +1,15 @@
+//
+//  omdbClient.swift
+//  Movie App
+//
+//  Created by Adrian Brown on 10/6/17.
+//  Copyright © 2017 Adrian Brown. All rights reserved.
+//
+
+import UIKit
+
+class omdbClient: NSObject {
+    
+//    static func
+
+}
