0d76dd43f3ed6766fe90c1fd419d4e04c3d6417b
Adrian Brown <adrian.angel.brown@gmail.com>
2018-04-10 14:21:06 -0400

    feature: created omdb api client

---
+++ Movie App/Slide.swift
@@ -0,0 +1,21 @@
+//
+//  Slide.swift
+//  Movie App
+//
+//  Created by Adrian Brown on 4/10/18.
+//  Copyright © 2018 Adrian Brown. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Movie App/omdbClient.swift
+++ Movie App/omdbClient.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import Alamofire
+
+struct OmdbClient {
+    
+    static func getMovies( completionHandler: @escaping ([String: Any]?, Error?) -> ()) {
+        let movieUrl = Constants.baseURL + Constants.slug + Constants.apiKey
+        print("Hello!!!", movieUrl)
+        Alamofire.request(movieUrl).responseJSON { response in
+            switch response.result {
+            case.success(let value):
+                completionHandler(value as? NSDictionary as! [String : Any]?, nil)
+            case .failure(let error):
+                completionHandler(nil, error)
+            }
+        }
+    }
+    
+//    static func getNextPassTime(lattitude: Double, longitude: Double,pin: Pin, completionHandler: @escaping([String: Any]?, Error?) -> ()) {
+//
+//        let passTimeURL = "\(Constants.baseURL)\(Constants.passTimeURL)lat=\(lattitude)&lon=\(longitude)"
+//        let queue = DispatchQueue(label: "BackgroundQueue", qos: .background, attributes: .concurrent)
+//        Alamofire.request(passTimeURL).responseJSON(queue: queue) { response in
+//            switch response.result {
+//            case.success(let value):
+//                completionHandler(value as? NSDictionary as! [String : Any]?, nil)
+//            case .failure(let error):
+//                completionHandler(nil, error)
+//            }
+//        }
+//    }
+}
